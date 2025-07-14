be040c5d7c0113f1e21624af4a089f95e681dc98
Franck Barbenoire <fbarbenoire@gmail.com>
2025-07-14 18:40:57 +0200

    correct cron scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,1 @@
-/usr/bin/sqlite3 /home/domotik/domotiq.db < /home/domotik/automations/scripts/cleanup.sql
+/usr/bin/sqlite3 /home/domotik/database/domotik.db < /home/domotik/automations/scripts/cleanup.sql
--- scripts/cleanup.sql
+++ scripts/cleanup.sql
@@ -1,5 +1,5 @@
-DELETE FROM linky WHERE timestamp < DATETIME('NOW', '-5 DAY');
-DELETE FROM on_off WHERE timestamp < DATETIME('NOW', '-5 DAY');
-DELETE FROM pressure WHERE timestamp <  DATETIME('NOW', '-5 DAY');
-DELETE FROM temperature_humidity WHERE timestamp <  DATETIME('NOW', '-5 DAY');
+DELETE FROM linky WHERE timestamp < STRFTIME('%s', 'NOW', '-5 DAY');
+DELETE FROM on_off WHERE timestamp < STRFTIME('%s', 'NOW', '-5 DAY');
+DELETE FROM pressure WHERE timestamp <  STRFTIME('%s', 'NOW', '-5 DAY');
+DELETE FROM temperature_humidity WHERE timestamp <  STRFTIME('%s', 'NOW', '-5 DAY');
 VACUUM;
